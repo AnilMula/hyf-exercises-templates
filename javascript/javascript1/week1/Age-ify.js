bea838c984ed25c5fd75a1741c27ad7cbcf3de60
AnilMula <anilreddymula@gmail.com>
2020-05-08 01:32:00 +0200

    opgave1 in javascript1 week1 homework is sent for feedback

---
+++ javascript/javascript1/week1/Age-ify.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var x = document.getElementById("YOB").value;
+    var y = document.getElementById("YF").value;
+    
+    document.getElementById("YA").value =  parseInt(y) - parseInt(x);
+  }
+       
