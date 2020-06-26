a9ebed868228af911d9166d63e32f721001c338e
AnilMula <anilreddymula@gmail.com>
2020-06-26 06:41:42 +0200

    added please move mouse

--- javascript/javascript2/week3/mousePosition.js
+++ javascript/javascript2/week3/mousePosition.js
@@ -6,17 +6,21 @@ document.addEventListener("mousemove", (event) => {
 
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 setInterval(() => {
-  const averageMousePosition =
-    "Xposition: " +
-    Math.trunc(
-      mousePosition.map((position) => position[0]).reduce(reducer, 0) /
-        mousePosition.length
-    ) +
-    "Yposition:" +
-    Math.trunc(
-      mousePosition.map((position) => position[1]).reduce(reducer, 0) /
-        mousePosition.length
-    );
-  window.alert(averageMousePosition);
+  if (mousePosition.length > 0) {
+    const averageMousePosition =
+      "Xposition: " +
+      Math.trunc(
+        mousePosition.map((position) => position[0]).reduce(reducer, 0) /
+          mousePosition.length
+      ) +
+      "Yposition:" +
+      Math.trunc(
+        mousePosition.map((position) => position[1]).reduce(reducer, 0) /
+          mousePosition.length
+      );
+    window.alert(averageMousePosition);
+  } else {
+    window.alert("please move the mouse");
+  }
   mousePosition = [];
 }, 5000);
