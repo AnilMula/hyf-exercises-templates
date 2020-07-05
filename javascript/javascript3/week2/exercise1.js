fa9958b75117e5499943be1583338c344953e4f2
AnilMula <anilreddymula@gmail.com>
2020-07-05 13:15:48 +0200

    exercise1 modified

--- javascript/javascript3/week2/exercise1.js
+++ javascript/javascript3/week2/exercise1.js
@@ -1,14 +1,23 @@
 // promise
-
 const promise = fetch("https://yesno.wtf/api");
 //using promise by async wait
 async function asyncAwaitExample() {
   try {
-    const data = await promise;
-    console.log(data);
-    console.log("yes");
+    const response = await promise;
+    /* promise way
+    const data = response.json().then((yesno) => {
+      console.log(yesno.answer); 
+          const h1 = document.getElementsByTagName("h1");
+    h1[0].innerText = yesno.answer;
+    });*/
+    const data = await response.json();
+
+    const h1 = document.getElementsByTagName("h1");
+    h1[0].innerText = data.answer;
+
+    console.log(data.answer);
   } catch (data) {
-    console.log("NO");
+    console.log("error");
   }
 }
 asyncAwaitExample();
---
+++ javascript/javascript3/week2/exercise5.js
@@ -0,0 +1,23 @@
+/* Using chaining
+
+Fetch the astronauts http://api.open-notify.org/astros.json
+After the astronauts has been fetched, fetch movies using this api
+https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json
+Log out the movies */
+
+//fetch astronauts data if we get a success message then fetch movies and display them
+fetch("http://api.open-notify.org/astros.json").then((response) => {
+  response.json().then((astronauts) => {
+    if (astronauts.message == "success") {
+      // astronauts data has been fetched successfully
+      //next fetch movies data
+      fetch(
+        "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json"
+      )
+        .then((response) => response.json())
+        .then((movies) => {
+          console.log(movies);
+        });
+    }
+  });
+});
